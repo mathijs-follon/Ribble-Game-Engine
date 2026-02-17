f265405a66150612c58d4394e6e3c0fef1459713
Mathijs Follon <mathijs.follon@gmail.com>
2026-02-17 14:26:30 +0100

    Add config header

---
+++ ribble/include/ribble/config/config.h
@@ -0,0 +1,14 @@
+//
+// Created by Mathijs Follon on 2/17/26.
+//
+
+#ifndef RIBBLE_CONFIG_H
+#define RIBBLE_CONFIG_H
+
+namespace ribble::config {
+        class Config {
+
+        };
+} // ribble::config
+
+#endif //RIBBLE_CONFIG_H
