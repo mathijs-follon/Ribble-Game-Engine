6b6c8e36f2407020f06680de5dfba565ed0e6345
Mathijs Follon <mathijs.follon@gmail.com>
2026-02-24 11:24:13 +0100

    Set todo in ribble.h

--- include/ribble.h
+++ include/ribble.h
@@ -1,8 +1,1 @@
-//
-// Created by Mathijs Follon on 2/19/26.
-//
-
-#ifndef RIBBLEENGINE_RIBBLE_H
-#define RIBBLEENGINE_RIBBLE_H
-
-#endif //RIBBLEENGINE_RIBBLE_H
+// TODO: Main Engine header file - includes all ribble classes
